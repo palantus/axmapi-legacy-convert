8715caff644e2d8dd7c44a22459e0de17cc20617
Anders <palantus@gmail.com>
2021-04-12 13:44:37 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# AXM Legacy Convert
+
+Create setup.json and fill with:
+
+- restartServices: boolean indicating if services from starter should be restarted
+- accessKey: API access key for legacy service
+- url: base URL to legacy service
+- destination: destination database path
---
+++ convert.js
@@ -0,0 +1,31 @@
+let fetch = require("node-fetch")
+let System = require("./lib/service")
+let Entity = require("entitystorage")
+const fs = require('fs');
+const setup = JSON.parse(fs.readFileSync('./setup.json',{encoding:'utf8', flag:'r'}));
+
+let convert = async () => {
+
+    if(setup.restartServices){
+      console.log("Stopping api service")
+      await fetch(`${setup.url}/api/disableService/axmanapi`)
+    }
+
+    console.log("Deleting current database")
+    try{require("fs").unlinkSync(`${setup.destination}/props.data`)}catch(err){}
+    try{require("fs").unlinkSync(`${setup.destination}/rels.data`)}catch(err){}
+    try{require("fs").unlinkSync(`${setup.destination}/tags.data`)}catch(err){}
+
+    console.log("Initializing entity")
+    await Entity.init(setup.destination)
+    
+    console.log("Converting data")
+    await new System(setup).convert()
+
+    if(setup.restartServices){
+      console.log("Starting service again")
+      await fetch(`${setup.url}/api/enableService/axmanapi`)
+    }
+}
+
+convert()
